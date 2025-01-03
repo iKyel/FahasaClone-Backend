5c1fe9ecd7692240616d8b5b97fad0e5b31b2f01
IAmBiny <anhtruong100703@gmail.com>
2025-01-03 16:33:37 +0700

    Hoàn thành chức năng Đăng ký, Đăng nhập và kiểm tra token của user

--- src/interface/AutheticatedRequest.ts
+++ src/interface/AutheticatedRequest.ts
@@ -1,6 +1,12 @@
-
 import { Request } from "express";
+import mongoose from "mongoose";
 
 export interface AuthenticatedRequest extends Request {
-
+    user?: {
+        _id: mongoose.Types.ObjectId,
+        hoDem: string,
+        ten: string,
+        userName: string,
+        loaiTK: string,
+    };
 }
--- src/routers/TaiKhoan.ts
+++ src/routers/TaiKhoan.ts
@@ -1,9 +1,18 @@
 import express from "express"
-import { registerAccount } from "../controllers/TaiKhoan";
+import { registerAccount, loginAccount } from "../controllers/TaiKhoan";
 
 const taiKhoanRouter = express.Router();
 
-// Route cho việc đăng ký tài khoản
+/**
+ * @route       POST '/api/taiKhoan/register'
+ * @description Đăng ký một tài khoản mới
+ */
 taiKhoanRouter.post('/register', registerAccount);
+
+/**
+ * @route       POST '/api/taiKhoan/login'
+ * @description Đăng nhập vào tài khoản
+ */
+taiKhoanRouter.post('/login', loginAccount);
 
 export default taiKhoanRouter;
